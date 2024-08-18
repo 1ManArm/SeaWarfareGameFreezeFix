4d60dda65b1f888018fa80fea34f6f8787d84cdf
Сергей <sergey.emelyanov.97@mail.ru>
2024-08-18 14:33:49 +0500

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SeaWarfareGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Model = new model();
+            Model.PlayerShips[0, 0] = CoordStatus.Ship;
+            Model.PlayerShips[5, 2] = CoordStatus.Ship;
+            Model.PlayerShips[5, 3] = CoordStatus.Ship;
+            Model.PlayerShips[5, 4] = CoordStatus.Ship;
+            Model.PlayerShips[7, 3] = CoordStatus.Ship;
+        }
+        model Model;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Model.LastShot = Model.Shot(textBox1.Text);
+            int x = int.Parse(textBox1.Text.Substring(0, 1));
+            int y = int.Parse(textBox1.Text.Substring(1));
+            switch (Model.LastShot)
+            {
+                case ShotStatus.Miss:
+                    Model.EnemyShips[x, y] = CoordStatus.Shot; break;
+                case ShotStatus.Wounded:
+                    Model.EnemyShips[x, y] = CoordStatus.Hit; break;
+                case ShotStatus.Kill:
+                    Model.EnemyShips[x, y] = CoordStatus.Hit; break;
+            }
+            if(Model.LastShot == ShotStatus.Wounded)
+            {
+                Model.LastShotCoord = textBox1.Text;
+                Model.WoundedStatus = true;
+            }
+            MessageBox.Show(Model.LastShot.ToString());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string s;
+            int x, y;
+            do
+            {
+                s = Model.ShotGen();
+                x = int.Parse(s.Substring(0, 1));
+                y = int.Parse(s.Substring(1));
+            }
+            while (Model.EnemyShips[x, y] != CoordStatus.None) ;
+            textBox1.Text = s;
+        }
+    }
+}
---
+++ model.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeaWarfareGame
+{
+    public enum ShotStatus 
+    { 
+        Miss, Wounded, Kill, EndBattle
+    }
+    public enum CoordStatus
+    {
+        None, Ship, Shot, Hit
+    }
+    public enum ShipType
+    {
+        x4, x3, x2, x1
+    }
+    public enum Direction
+    {
+        Horizontal, Vertical
+    }
+    public class model
+    {
+        //Массив кораблей игрока
+        public CoordStatus [,] PlayerShips = new CoordStatus[10,10];
+        //Массив кораблей противника
+        public CoordStatus [,] EnemyShips = new CoordStatus[10, 10];
+        
+        //Кол-во необнаруженных клеток противника
+        public int UndiscoveredCells = 20;
+        
+        //Поле статуса последнего выстрела
+        public ShotStatus LastShot;
+        //Поле статуса ранения
+        public bool WoundedStatus;
+        //Поле статуса первого попадания
+        public bool FirstHit;
+        //Поле координат последнего выстрела
+        public string LastShotCoord;
+        //Метод проверки смежных клеток
+        public void CheckAdjacentCells(ref int x, ref int y) { }
+        //Метод исключения уже совершенных попаданий
+        public void AvoidMissedShots(ref int x, ref int y) {  }
+
+        //Конструктор. Инициализация полей модели
+        public model()
+        {
+            LastShot = ShotStatus.Miss;
+            WoundedStatus = false;
+            for (int i = 0; i < 10;  i++)
+                for (int j = 0; j < 10; j++)
+                {
+                    PlayerShips[i, j] = CoordStatus.None;
+                    EnemyShips[i, j] = CoordStatus.None;
+                }
+        }
+        //Выстрел игрока. Входящий параметр - координаты выстрела в виде строки из 2х цифр
+        public ShotStatus Shot(string ShotCoord)
+        {
+            ShotStatus result = ShotStatus.Miss;
+            int x, y;
+            x = int.Parse(ShotCoord.Substring(0, 1));
+            y = int.Parse(ShotCoord.Substring(1));
+            if (PlayerShips[x, y] == CoordStatus.None)
+                { result = ShotStatus.Miss; }
+            else 
+            { 
+                result = ShotStatus.Kill;
+                if ((x != 9 && PlayerShips[x + 1, y] == CoordStatus.Ship) ||
+                    (y != 9 && PlayerShips[x, y + 1] == CoordStatus.Ship) ||
+                    (x != 0 && PlayerShips[x - 1, y] == CoordStatus.Ship) ||
+                    (y != 0 && PlayerShips[x, y - 1] == CoordStatus.Ship)||
+                    (x < 8 && PlayerShips[x + 2, y] == CoordStatus.Ship) ||
+                    (y < 8 && PlayerShips[x, y + 2] == CoordStatus.Ship) ||
+                    (x > 1 && PlayerShips[x - 2, y] == CoordStatus.Ship) ||
+                    (y > 1 && PlayerShips[x, y - 2] == CoordStatus.Ship) ||
+                    (x < 7 && PlayerShips[x + 3, y] == CoordStatus.Ship) ||
+                    (y < 7 && PlayerShips[x, y + 3] == CoordStatus.Ship) ||
+                    (x > 2 && PlayerShips[x - 3, y] == CoordStatus.Ship) ||
+                    (y > 2 && PlayerShips[x, y - 3] == CoordStatus.Ship) /*||*/
+                    //(x < 6 && PlayerShips[x + 4, y] == CoordStatus.Ship) ||
+                    //(y < 6 && PlayerShips[x, y + 4] == CoordStatus.Ship) ||
+                    //(x > 3 && PlayerShips[x - 4, y] == CoordStatus.Ship) ||
+                   /* (y > 3 && PlayerShips[x, y - 4] == CoordStatus.Ship)*/)
+                    result = ShotStatus.Wounded;
+                PlayerShips[x, y] = CoordStatus.Hit;
+                UndiscoveredCells--;
+                if (UndiscoveredCells == 0)
+                {
+                    result = ShotStatus.EndBattle;
+                }
+
+            }
+            return result;
+        }
+
+        //Генерация выстрела
+        public string ShotGen()
+        {
+            Random rand = new Random();
+            int x, y;
+
+            for (int attempt = 0; attempt < 100; attempt++)
+            {
+                x = rand.Next(0, 10);
+                y = rand.Next(0, 10);
+
+                if (EnemyShips[x, y] == CoordStatus.None)
+                {
+                    LastShotCoord = $"{x}{y}";
+                    return LastShotCoord;
+                }
+            }
+
+            throw new InvalidOperationException("Нет доступных координат для выстрела.");
+        }
+    }
+}
